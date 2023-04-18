6e6cee102ccb4a28fe0c78ea65f5df0353c1db93
bedettina <bforbedetti@gmail.com>
2023-04-18 17:37:05 -0300

    refactoring code

--- api/src/db.js
+++ api/src/db.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+require('dotenv').config();
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 const modelPokemon = require ('./models/Pokemon');
 const modelType = require ('./models/Type');
 
 const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
+   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
    {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
